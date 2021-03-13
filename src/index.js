77a8170442524030a339eaffab96691ca1d3744f
Sergey Mangilev <ssmangilev@gmail.com>
2021-03-13 07:43:03 +0100

    init: task 'brackets' solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length %2 !=0) {
+        return false;
+    }
+    const open = bracketsConfig.map( currentValue => currentValue[0])
+    const close = bracketsConfig.map( currentValue => currentValue[1])
+    let checked_brackets = [];
+    for (let i=0; i<str.length; i++) {
+      if (i === 0 && !open.includes(str[i])) {
+        return false;
+      }
+      else if (i === str.length-1 && !close.includes(str[i])) {
+          return false;
+      }
+      else if (close.includes(str[i]) && checked_brackets[checked_brackets.length-1] == open[close.indexOf(str[i])] ) {
+        checked_brackets.pop()
+      }
+      else if (open.includes(str[i])) {
+        checked_brackets.push(str[i])
+      }
+    }
+    if (checked_brackets.length > 0) {
+      return false
+    }
+    else {return true}
+  }
